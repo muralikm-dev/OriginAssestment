601e93a19bdfa7ef8ae68d10c668370c18d174ab
muralikm-dev <56745045+muralikm-dev@users.noreply.github.com>
2020-11-28 23:42:48 +1000

    Update fetchRestApi.ts

--- src/redux/userdetails/fetchRestApi.ts
+++ src/redux/userdetails/fetchRestApi.ts
@@ -1,8 +1,9 @@
 export const fetchRestApi = async () => {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const response = await fetch("https://z2wzh9du86.execute-api.ap-southeast-2.amazonaws.com/live/users");
+            // "https://jsonplaceholder.typicode.com/users");            
         return response.json()
     } catch (e) {
-        
+        console.log(e);
     }
 }
