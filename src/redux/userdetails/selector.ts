f0a1b419b3f85471a4bd1757057f34606b2e941f
muralikm-dev <56745045+muralikm-dev@users.noreply.github.com>
2020-11-30 01:54:12 +1000

    Update selector.ts

--- src/redux/userdetails/selector.ts
+++ src/redux/userdetails/selector.ts
@@ -1,11 +1,13 @@
 import { createSelector } from "reselect";
-import { UserDetailsState, UserDetails } from "./types";
+import { UserDetailsState, UserDetails, AwsDetails } from "./types";
 import { RootState } from "../rootReducer";
 
 const userDetailsList = (state: RootState): UserDetails[] => state.userDetails.data;
 const updatedDetailsList = (state: RootState): UserDetails[] => state.userDetails.data;
 const updatedDetailsUpdated = (state: RootState): boolean => state.userDetails.updated;
+const awsDetails = (state: RootState): AwsDetails => state.awsDetails.data;
 
 export const userDetailsSelector = createSelector(userDetailsList, userDetails => userDetails);
 export const updatedDetailsSelector = createSelector(updatedDetailsList, updateDetails => updateDetails)
 export const updatedSelector = createSelector(updatedDetailsUpdated, updated => updated);
+export const awsDetailsSelector = createSelector(awsDetails, awsDetail => awsDetail);
