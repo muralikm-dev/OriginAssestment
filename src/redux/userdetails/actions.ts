d422709ceee084994fe929cad09e20dfd827f531
muralikm-dev <56745045+muralikm-dev@users.noreply.github.com>
2020-11-30 01:47:11 +1000

    Update actions.ts

--- src/redux/userdetails/actions.ts
+++ src/redux/userdetails/actions.ts
@@ -1,19 +1,35 @@
 import { FETCH_DETAILS, FetchDetailsAction, UPDATE_DETAILS, 
   UpdateDetailsAction, UserDetails, FetchDetailsSuccessAction, 
   FETCH_DETAILS_SUCCESS, FetchDetailsFailureAction, FETCH_DETAILS_FAILURE, 
-  UPDATE_DETAILS_SUCCESS, UPDATE_DETAILS_FAILURE, UpdateDetailsSuccessAction, UpdateDetailsFailureAction } from './types';
+  UPDATE_DETAILS_SUCCESS, UPDATE_DETAILS_FAILURE, UpdateDetailsSuccessAction, UpdateDetailsFailureAction,
+  FETCH_AWS_REGION, FETCH_AWS_REGION_SUCCESS, FETCH_AWS_REGION_FAILURE,
+  FetchAWSDetailAction, FetchAWSDetailSuccessAction, FetchAWSDetailFailureAction, AwsDetails } from './types';
 
 export const fetchDetails = (): FetchDetailsAction => ({
     type: FETCH_DETAILS,
 });
 
-export const fetchParListingSuccess = (payload: UserDetails[]): FetchDetailsSuccessAction => ({
+export const fetchDetailsSuccess = (payload: UserDetails[]): FetchDetailsSuccessAction => ({
   type: FETCH_DETAILS_SUCCESS,
   payload
 });
 
 export const fetchDetailsFailureAction = (error: Error): FetchDetailsFailureAction => ({
   type: FETCH_DETAILS_FAILURE,
+  payload: error
+});
+
+export const fetchAwsDetails = (): FetchAWSDetailAction => ({
+  type: FETCH_AWS_REGION,
+});
+
+export const fetchAwsDetailsSuccess = (payload: AwsDetails): FetchAWSDetailSuccessAction => ({
+  type: FETCH_AWS_REGION_SUCCESS,
+  payload
+});
+
+export const fetchAwsDetailsFailure = (error: Error): FetchAWSDetailFailureAction => ({
+  type: FETCH_AWS_REGION_FAILURE,
   payload: error
 });
 
